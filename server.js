16d670c40c9282760d63231493e7881594184851
ceulain <ceul1barth@hotmail.fr>
2016-09-23 23:12:23 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(process.env.PORT || 8080);
