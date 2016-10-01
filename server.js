d9fe6214f209b9734a31e3164d9eb5a966a60337
Sabine Caizergues <sabinecaizergues@gmail.com>
2016-10-01 13:16:24 +0200

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-'use strict'
+'use strict';
 
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let port = process.env['PORT'] || 8080;
+let nodemailer = require('nodemailer');
+let mg = require('nodemailer-mailgun-transport');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
@@ -16,6 +18,42 @@ app.get('/', (req, res) => {
 app.get('/reset_password/:token', (req, res) => {
 
     res.sendFile('public/views/index_password.html', { root: __dirname });
+});
+
+app.post('/email', (req, res) => {
+   console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.body);
+
+    var auth = {
+
+        auth : {
+            api_key:'key-2cbf56735c697b79b2b69306c4d0b79c',
+            domain: 'playerz.io'
+        }
+
+
+    };
+    console.log(auth);
+
+
+    var transporter = nodemailer.createTransport(mg(auth));
+    let mailOptions = {
+        to: 'contact@playerz.io',
+        from: req.body.email,
+        subject: '[Formulaire de contact] nouveau message de '+ req.body.name,
+        text: req.body.body
+    };
+
+    transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+           console.log('Server is ready to take our messages');
+            res.redirect('/');
+            // TODO : REDIRECT TO A MODAL TO CONFIRM SENDING EMAIL
+        }
+    });
 });
 
 app.listen(port);
