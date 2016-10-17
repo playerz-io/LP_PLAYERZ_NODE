2a38def55fc4c26c5f19a2d50c00d5839ab380ac
ceulain <ceul1barth@hotmail.fr>
2016-10-17 11:10:39 +0200

    add task fonts in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -16,41 +16,48 @@ gulp.task('js', function() {
 });
 
 // Tâche "img" = Images optimisées
-gulp.task('img', function () {
+gulp.task('img', function() {
     return gulp.src(source + '/assets/img/*.{jpg,jpeg,gif,svg,png}')
         .pipe(plugins.imagemin()) // optimisation
         .pipe(gulp.dest(destination + '/assets/img'));
 });
 
 // Tâche "build" = CSS + autoprefixer + CSScomb + beautify (source -> destination)
-gulp.task('css', function () {
-   return gulp.src(source + '/assets/css/*.css')
-       .pipe(plugins.cssbeautify({indent: '  '}))  //reformate et ré-indent le css
-       .pipe(plugins.autoprefixer()) // ajoute les prefixes CSS3
-       .pipe(plugins.uncss({
-           html: [source + '/views/*.html']
-       })) //  supprime les styles css non utilisés
-       .pipe(gulp.dest(destination + '/assets/css/'));
+gulp.task('css', function() {
+    return gulp.src(source + '/assets/css/*.css')
+        .pipe(plugins.cssbeautify({
+            indent: '  '
+        })) //reformate et ré-indent le css
+        .pipe(plugins.autoprefixer()) // ajoute les prefixes CSS3
+        .pipe(plugins.uncss({
+            html: [source + '/views/*.html']
+        })) //  supprime les styles css non utilisés
+        .pipe(gulp.dest(destination + '/assets/css/'));
 });
 
 // Tâche "minify" = minification CSS (destination -> destination)
-gulp.task('minify', function () {
-   return gulp.src(destination + '/assets/css/*.css')
-       .pipe(plugins.csso())
-       .pipe(plugins.rename({
-           suffix: '.min'
-       }))
-       .pipe(gulp.dest(destination + '/assets/css/'));
+gulp.task('minify', function() {
+    return gulp.src(destination + '/assets/css/*.css')
+        .pipe(plugins.csso())
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(destination + '/assets/css/'));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src(source + '/assets/fonts/*')
+        .pipe(gulp.dest(destination + '/assets/fonts'));
 });
 
 //tache "build"
 gulp.task('build', ['css']);
 
 //tache "prod" = Build + minify + js + img
-gulp.task('prod', gulpsync.sync(['build', 'minify', 'js', 'img']));
+gulp.task('prod', gulpsync.sync(['build', 'minify', 'js', 'img', 'fonts']));
 
 //tache "watch" = je surveille *css et js
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch(source + '/assets/css/*.css', ['build']);
     gulp.watch(source + '/assets/js/*.js', ['js']);
     gulp.watch(source + '/assets/img/*', ['img']);
